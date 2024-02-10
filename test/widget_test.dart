768bc72590e1d137ed882258c4086b126dd5744d
Moamn <moamnhonor@gmail.com>
2024-02-10 19:39:46 +0200

    Icon Page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:technoadala/main.dart';
+import 'package:techno3adalaa/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
