d886c8642508174e96e620fd06fa78a9e31efe83
Omar Amr <139566672+omr-khtb@users.noreply.github.com>
2024-02-17 10:21:37 +0200

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+ // Firebase.initializeApp(
+//    options: DefaultFirebaseOptions.currentPlatform,
+// );
   runApp(MyApp());
+  
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
